7a2b3bda9d4b4e556ace812c5654839166d7aff3
rafael <rafael.simas@al.resilia.com.br>
2022-09-04 10:16:24 -0300

    login

---
+++ assets/js/funcionalidades.js
@@ -0,0 +1,6 @@
+function buscar(){
+    const quartaTela = document.querySelector('.quarta-tela')
+    const quintaTela = document.querySelector('.quinta-tela')
+    quartaTela.classList.add('escondido')
+    quintaTela.classList.remove('escondido')
+}
--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,29 +1,65 @@
-let usuarioLogado;
 
-function login(){
-    const btnLogin = document.querySelector('.btn-login-pagina-login')
+let nome;
+
+const btn1 = document.querySelector('.btn-login')
+btn1.addEventListener('click', function(){
     const primeiraTela = document.querySelector('.primeira-tela')
     const segundaTela = document.querySelector('.segunda-tela')
     primeiraTela.classList.add('escondido')
     segundaTela.classList.remove('escondido')
-}
+})
 
-function entrar(){
+const btnAcesso = document.querySelector('.btn-login-usuario')
+btnAcesso.addEventListener('click', function(){
+    const inputLogin = document.querySelector('#inputLogin').value
+    const user = document.querySelector('#user')
+    user.innerHTML = inputLogin
+
     const segundaTela = document.querySelector('.segunda-tela')
     const quartaTela = document.querySelector('.quarta-tela')
     segundaTela.classList.add('escondido')
     quartaTela.classList.remove('escondido')
-    const inputCampoUsuario = document.querySelector('.btn-login-pagina-login').value
-    const user = document.querySelector('#user')
-    user.innerHTML = inputCampoUsuario
-    registrarUsuario = user
 
-}
+    nomeDoUsuario.innerHTML = campoNomeDoUsuario
+
+    nome = nomeDoUsuario
+})
 
 
-function registrarUsuario(){
+const btn2 = document.querySelector('.btn-registro')
+btn2.addEventListener('click', function(){
     const primeiraTela = document.querySelector('.primeira-tela')
     const terceiraTela = document.querySelector('.terceira-tela')
     primeiraTela.classList.add('escondido')
     terceiraTela.classList.remove('escondido')
+})
+
+
+
+function registrar(){
+    const voltarPHome = document.querySelector('.voltarPHome')
+    voltarPHome.innerHTML = 'Armazenando suas informações'
+    voltarPHome.style.backgroundColor = 'yellow'
+    voltarPHome.style.color = 'black'
+    voltarPHome.style.padding = '19px'
+    setTimeout(sucesso, 2000)
+    setTimeout(voltarParaHome, 3000)
 }
+
+function sucesso(){
+    const voltarPHome = document.querySelector('.voltarPHome')
+    voltarPHome.innerHTML = 'Informações armazenadas com sucesso'
+    voltarPHome.style.backgroundColor = 'green'
+    voltarPHome.style.color = 'white'
+    voltarPHome.style.padding = '19px'
+}
+
+function voltarParaHome(){
+    const primeiraTela = document.querySelector('.primeira-tela')
+    const terceiraTela = document.querySelector('.terceira-tela')
+    primeiraTela.classList.remove('escondido')
+    terceiraTela.classList.add('escondido')
+}
+
+
+
