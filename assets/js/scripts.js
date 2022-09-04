6b2f48f2654f23af5dd1cf0c8e3af350adbae23b
rafael <rafael.simas@al.resilia.com.br>
2022-09-04 07:43:51 -0300

    Adicionada tela de busca

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,3 +1,5 @@
+let usuarioLogado;
+
 function login(){
     const btnLogin = document.querySelector('.btn-login-pagina-login')
     const primeiraTela = document.querySelector('.primeira-tela')
@@ -5,6 +7,19 @@ function login(){
     primeiraTela.classList.add('escondido')
     segundaTela.classList.remove('escondido')
 }
+
+function entrar(){
+    const segundaTela = document.querySelector('.segunda-tela')
+    const quartaTela = document.querySelector('.quarta-tela')
+    segundaTela.classList.add('escondido')
+    quartaTela.classList.remove('escondido')
+    const inputCampoUsuario = document.querySelector('.btn-login-pagina-login').value
+    const user = document.querySelector('#user')
+    user.innerHTML = inputCampoUsuario
+    registrarUsuario = user
+
+}
+
 
 function registrarUsuario(){
     const primeiraTela = document.querySelector('.primeira-tela')
